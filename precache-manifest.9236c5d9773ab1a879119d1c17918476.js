859ac8413b69e108ad9e5677aeef7e96f75ad18b
Devanshu Sanghani <devanshu.sanghani@gmail.com>
2020-08-14 18:27:05 +0530

    Updates

---
+++ precache-manifest.9236c5d9773ab1a879119d1c17918476.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca3336791e712f9d064aab296e0a68bd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85347c294d4e4940e5c0",
+    "url": "/static/css/main.498d002a.chunk.css"
+  },
+  {
+    "revision": "3602b91065bc61b04ebb",
+    "url": "/static/js/2.1e9d9dc5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.1e9d9dc5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85347c294d4e4940e5c0",
+    "url": "/static/js/main.75ab00ef.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "64d31178ec1b2547d5f4368fe425f7d8",
+    "url": "/static/media/profile.64d31178.jpg"
+  }
+]);
